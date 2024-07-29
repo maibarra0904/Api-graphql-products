2d9a6a5bac57c313b244a6098ce46522deb2600f
Mario Ibarra <mario.ibarra.86@gmail.com>
2024-07-29 10:43:57 -0500

    Implementando Ruta y Controlador de productos asi como configurando la BD Mysql con Docker

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import  server from './server';
 import cors from "cors"
 import dotenv from 'dotenv';
+import { ProductRouter } from './routes/products.route';
+
+
 dotenv.config()
 
 const port = +process.env.PORT || 4000;
@@ -8,6 +11,8 @@ const port = +process.env.PORT || 4000;
 server.use(cors());
 
 //server.use(express.json());
+
+server.use("/products", ProductRouter)
 
 
 server.listen(port, () => {
