f150969768776bc80fd0a5a21775095b3c7f899c
Mario Ibarra <mario.ibarra.86@gmail.com>
2024-07-30 11:50:36 -0500

    Conexion a BD con Sequelize

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import  server from './server';
 import cors from "cors"
 import dotenv from 'dotenv';
 import { ProductRouter } from './routes/products.route';
+import db from './config/database';
 
 
 dotenv.config()
@@ -15,6 +16,12 @@ server.use(cors());
 server.use("/products", ProductRouter)
 
 
-server.listen(port, () => {
+server.listen(port, async() => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
+
+  try {
+    await db.authenticate()
+  } catch (error) {
+    console.log(error)
+  }
 });
