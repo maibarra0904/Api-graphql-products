9ad43dab9b320bb8e93e74ac7b4ce07828e066a9
Mario Ibarra <mario.ibarra.86@gmail.com>
2024-07-31 09:52:13 -0500

    Realizando la configuracion de los archivos de docker para crear la imagen de la aplicación

--- src/config/database.ts
+++ src/config/database.ts
@@ -10,4 +10,5 @@ const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.D
 });
 
 
+
 export default db;
--- src/index.ts
+++ src/index.ts
@@ -3,30 +3,28 @@ import cors from "cors"
 import dotenv from 'dotenv';
 import { ProductRouter } from './routes/products.route';
 import db from './config/database';
-import Product from './models/products.model';
 import express from 'express';
 
 
 dotenv.config()
 
-const port = +process.env.PORT || 4000;
+const port = +process.env.PORT || 3000;
 
 server.use(cors());
 server.use(express.json());
 
-//server.use(express.json());
 
 server.use("/products", ProductRouter)
 
 
 
-db.sync({ force: true })
+db.sync({ force: false })
   .then(() => {
     console.log("Tablas sincronizadas con la base de datos.");
 
     // Iniciar el servidor
     server.listen(port, () => {
-      console.log("Servidor iniciado en el puerto 4000.");
+      console.log(`Servidor iniciado en el puerto ${port}`);
     });
   })
   .catch((error) => {
